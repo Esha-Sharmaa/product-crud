de9b20160ee5b410e722bb89f9f9fe12dd3c535d
Esha Sharma <eshasharmakb22@gmail.com>
2023-12-23 15:16:11 +0530

    written the code to connet to mysql database and reterive all the products form products table in showed them on the index.php page

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
+
+<?php
+try {
+    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', '');
+    // if there was an error establishing connection 
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $query = $pdo->prepare('SELECT * FROM products ORDER BY title ');
+    $query->execute();
+    $products = $query->fetchAll(PDO::FETCH_ASSOC);
+    // echo '<pre>';
+    // var_dump($products);
+    // echo '</pre>';
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
 <!doctype html>
-<html lang="ar" dir="rtl">
+<html lang="eng" >
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -7,15 +23,43 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="app.css">
     <title> PhP Prdocut Crud Application </title>
   </head>
   <body>
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <h1> Product Details</h1>
+    <a href = "./create.php" class="btn btn-success"> Add Product</a>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope = "col" > # </th> 
+      <th scope="col">Image</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Price</th>
+      <th scope="col">Create Date</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    
+    <?php foreach ($products as $i => $product): ?>
+                    <tr>
+                  <th scope="row"> <?php echo $i + 1; ?> </th>
+                  <td><img src="<?php echo $product["image"]; ?>" alt = "image" width=80px height= 80px /></td>
+                  <td> <?php echo $product["title"]; ?></td>
+                  <td><?php echo $product["description"]; ?></td>
+                  <td><?php echo $product["price"]; ?></td>
+                  <td><?php echo $product["create_date"]; ?></td>
+                  <td>
+                    <button type="button" class="btn btn-sm btn-outline-success">Edit</button>
+                    <button type="button" class="btn btn-sm btn-outline-danger">Danger</button>
+                 </td>
+                </tr>
+    
+    <?php endforeach; ?>
+    
+  </tbody>
+</table>
   </body>
 </html> 
